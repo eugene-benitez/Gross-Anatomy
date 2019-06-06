a10bf94ca5a820fbcb6b1bb0808eb1b5b0b8b231
eugene-benitez <eugenebenitez@protonmail.com>
2019-06-06 15:29:26 -0700

    Completed MongoDB Integration and API. Form is tested including validations. Defaulted muscle region in component.ts to PECTORAL for ease of data-entry.

--- public/src/app/common/components/home/home.component.ts
+++ public/src/app/common/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { newMuscle } from '../../models/createMuscle';
+import { MuscleError } from '../../models/muscleError';
+import { Router } from '@angular/router';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  newMuscle: newMuscle = {
+    name: '',
+    origin: '',
+    insertion: '',
+    nerveSupply: '',
+    action: '',
+    region: '',
+    compartment: '',
+  }
+
+  MuscleError: MuscleError = {
+    name: '',
+    origin: '',
+    insertion: '',
+    nerveSupply: '',
+    action: '',
+    region: '',
+    compartment: '',
+  }
+
+  constructor(
+    private router: Router,
+    private http: HttpService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onCreate() {
+
+    //! DEFAULTED region to Pectoral for ease of data-entry
+
+    this.newMuscle.region = 'Pectoral';
+    this.http.addMuscle(this.newMuscle)
+      .subscribe(data => {
+        this.MuscleError = {
+          name: '',
+          origin: '',
+          insertion: '',
+          nerveSupply: '',
+          action: '',
+          region: '',
+          compartment: '',
+        };
+
+        if (!data['errors']) {
+          this.router.navigateByUrl('');
+        } else {
+          for (let k in data['errors']) {
+            this.MuscleError[k] = data['errors'][k]['message'];
+          }
+        }
+
+
+      });
+
   }
 
 }
